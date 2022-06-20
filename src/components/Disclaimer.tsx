c5f330bfd048baa179835c07b48d7b21ab1cc9be
yc2288 <yc2288@cornell.edu>
2022-06-20 11:24:55 -0700

    disclaimer update

--- src/components/Disclaimer.tsx
+++ src/components/Disclaimer.tsx
@@ -14,12 +14,15 @@ const Disclaimer = (props: DisclaimerProps) => {
         <ModalBody>
           <Heading mt="8" mb="10" fontSize="64px" textAlign="center" fontWeight={400}>Disclaimer</Heading>
           <Box px="16" mb="16" fontSize="sm">
-            <Text mb="4">Because XRPL NFT-Dev net is under development, the Itoka IC-XRP cross-chain project is for R&D purpose, and the Itoka team airdropped the Bazahei NFTs to the community for free. The <strong>XRPL NFT-Dev net will be reset and merged</strong> to the main net in the future. Please pay attention to the Ripple Labs announcement.</Text>
-            <Text mb="4" fontWeight={700}>XRPL NFT-Dev is for experiencing only. We extremely recommend stake NFT assets on the Internet Computer network.</Text>
-            <Text mb="4">The investors must conduct their own research before trading. Itoka and OctAI Inc. do not accept any responsibility or liability for any loss of assets investment.</Text>
+            <Text mb="4">The Itoka IC-XRP cross-chain project is for R&D purposes only. Since the XRPL NFT-Dev net is still under development and will be
+               <strong>reset and merged to the main net </strong> in the future, we <strong> highly recommend storing NFT assets on the Internet Computer network. </strong> 
+               Please pay attention to the Ripple Labs announcement. Itoka and OctAI Inc. do not accept any responsibility or liability for any loss of assets caused by XRPL updates.
+          </Text>
+            <Text mb="4" fontWeight={700}>The Itoka team airdropped the Bazahei NFTs to the community for free. </Text>
+            <Text mb="4">Investors must conduct their own research before trading. Itoka and OctAI Inc. do not accept any responsibility or liability for any loss of assets or investments.</Text>
           </Box>
           <Center py="10">
-            <Checkbox onChange={() => props.onClose()}>I have read, understand, and agree to the above</Checkbox>
+            <Checkbox onChange={() => props.onClose()}>I have read, understood, and agree to the above</Checkbox>
           </Center>
         </ModalBody>
       </ModalContent>
